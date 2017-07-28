bc3925aecff88236292d8b633e59691053b62524
Dorian Eikenberg <deikenberg@cocus.com>
2017-07-28 15:42:49 +0200

    renamed ircbot.py

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ import stat
 from IRCBot import IRCBot
 
 SERVERADDRESS = "192.168.56.101"
-UDS_FILE = "/run/monitorbot"
+UDS_FILE = "/home/rageagainsthepc/monitorbot"
 
 
-def handleConnection(clientSocket):
-    return "nothing"
+def handleConnection(bot, clientSocket):
+    with clientSocket.makefile("r") as fd:
+        msg = []
+        msg.extend(fd.readlines())
+        for line in msg:
+            bot.composeMsgToChannel("test", line)
 
 
+
+# Ich erwarte im Moment nicht mehr als eine Connection auf einmal.
+# könnte man bei Gelegenheit aber mal mit threading.Thread(target=handleConnection, (clientSocket, )) machen...
 def runServer(bot):
     try:
         os.unlink(UDS_FILE)
@@ -23,15 +30,13 @@ def runServer(bot):
     server_socket.listen(1)
     while True:
         clientSocket, clientAddress = server_socket.accept()
-        handleConnection(clientSocket)
+        handleConnection(bot, clientSocket)
 
 
 def main():
     bot = IRCBot(SERVERADDRESS)
     bot.run()
-    while (True):
-        print("yay")
-        time.sleep(3)
+    runServer(bot)
 
 
 if __name__ == "__main__":
