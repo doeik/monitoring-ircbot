2c2827cfbfc783b9e7898d0ac9f20ab6fcb79706
Dorian Eikenberg <deikenberg@cocus.com>
2017-07-28 11:31:20 +0200

    refactored bot into a class

--- IRCBot.py
+++ IRCBot.py
@@ -1,11 +1,87 @@
 import socket
+import threading
 import traceback
 
+CREDENTIALS = "NICK Bot\r\n" \
+              "USER blaBot * 8 :testBOT\r\n"
+
 class IRCBot:
+    serverAddress = None
     clientSocket = None
     fd = None
+    running = True
+    lock = None
 
     def __init__(self, address):
+        self.serverAddress = address
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clientSocket.connect((address, 6667))
         self.fd = self.clientSocket.makefile("r")
+        self.lock = threading.Lock()
+
+
+    def quit(self):
+        self.sendMsg("QUIT")
+        try:
+            self.clientSocket.close()
+            self.fd.close()
+        except:
+            pass
+
+
+    def __del__(self):
+        self.quit()
+
+
+    def sendMsg(self, msg):
+        with self.lock:
+            try:
+                self.clientSocket.send((msg + "\r\n").encode('UTF-8'))
+            except Exception:
+                traceback.print_exc()
+
+
+
+    def waitForEvent(self, action):
+        result = False
+        while not result:
+            received = self.fd.readline()
+            # super fancy println debugging
+            print(received)
+            result = action(received)
+        return result
+
+
+    def sendPong(self, received):
+        res = False
+        if received.startswith("PING"):
+            self.sendMsg("PONG" + received[4:])
+            res = True
+        return res
+
+
+    def joinChannel(self, received):
+        res = False
+        tmp = received.split()[1]
+        if (tmp == "376" or tmp == "422"):
+            self.sendMsg("JOIN #test")
+            res = True
+        return res
+
+
+    def run(self):
+        t = threading.Thread(target=self._run)
+        t.start()
+
+
+    def _run(self):
+        try:
+            self.clientSocket.connect((self.serverAddress, 6667))
+        except Exception:
+            traceback.print_exc()
+        else:
+            with self.fd:
+                self.sendMsg(CREDENTIALS)
+                self.waitForEvent(self.sendPong)
+                self.waitForEvent(self.joinChannel)
+                while (self.running):
+                    self.waitForEvent(self.sendPong)
--- ircbot.py
+++ ircbot.py
@@ -1,53 +1,38 @@
 import socket
-import traceback
 import time
+import os
+import stat
+from IRCBot import IRCBot
 
 
 SERVERADDRESS = "192.168.56.101"
-CREDENTIALS = "NICK Bot\r\n" \
-              "USER blaBot * 8 :testBOT\r\n"
+UDS_FILE = "/run/monitorbot"
 
 
-def waitForEvent(fd, socket, action):
-    result = None
-    while(result == None):
-        test = fd.readline()
-        print(test)
-        result = action(socket, test)
-    return result
+def handleConnection(clientSocket):
 
 
-def sendPong(socket, test):
-    res = None
-    if test.startswith("PING"):
-        socket.send(("PONG" + test[4:]).encode('UTF-8'))
-        res = "yay"
-    return res
 
-
-def joinChannel(socket, test):
-    res = None
-    tmp = test.split()[1]
-    if (tmp == "376" or tmp == "422"):
-        socket.send("JOIN #test\r\n".encode('UTF-8'))
-        res = "yay"
-    return res
+def runServer(bot):
+    try:
+        os.unlink(UDS_FILE)
+    except OSError:
+        pass
+    server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    server_socket.bind(UDS_FILE)
+    os.chmod(UDS_FILE, stat.S_IRWXU | stat.S_IRGRP | stat.S_IWGRP)
+    server_socket.listen(1)
+    while True:
+        clientSocket, clientAddress = server_socket.accept()
+        handleConnection(clientSocket)
 
 
 def main():
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        clientSocket.connect((SERVERADDRESS, 6667))
-    except Exception:
-        traceback.print_exc()
-    else:
-        with clientSocket.makefile("r") as fd:
-            clientSocket.send(CREDENTIALS.encode('UTF-8'))
-            waitForEvent(fd, clientSocket, sendPong)
-            waitForEvent(fd, clientSocket, joinChannel)
-            while(True):
-                waitForEvent(fd, clientSocket, sendPong)
-
+   bot = IRCBot(SERVERADDRESS)
+   bot.run()
+   while(True):
+       print("yay")
+       time.sleep(3)
 
 
 
